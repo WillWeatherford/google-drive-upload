0a7dcc5735c6b523493274a8223baa574c024aa1
Will Weatherford <weatherford.william@gmail.com>
2016-09-23 19:46:49 -0700

    Changed permission scope to drive.file.

--- quickstart.py
+++ quickstart.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+SCOPES = 'https://www.googleapis.com/auth/drive.file'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 
