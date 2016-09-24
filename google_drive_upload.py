64d8f4c93db82a6b5b891aee31298a21d4781f34
Will Weatherford <weatherford.william@gmail.com>
2016-09-23 17:37:56 -0700

    Better ValueError message when not a real directory.

--- google_drive_upload.py
+++ google_drive_upload.py
@@ -5,6 +5,6 @@ import os
 def iter_directory(path):
     """Generate the files from a given directory path."""
     if not os.path.isdir(path):
-        raise ValueError('Given directory path is not a directory.')
+        raise ValueError('No such directory: {}'.format(path))
     for filename in os.listdir(path):
         yield filename
